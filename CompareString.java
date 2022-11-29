8d6ea2e4efa66c6f0c742c0ebfe27d3b2363af46
Ataliya Aarfeen <89702899+Ataliya-01@users.noreply.github.com>
2022-11-29 21:36:04 +0530

    Add files via upload

---
+++ CompareString.java
@@ -0,0 +1,20 @@
+// Compare 2 strings
+import java.util.Scanner;
+public class CompareString{
+	
+	public static void main(String args[]){
+		
+		String name1 = "Ataliya";
+		String name2 = "Ataliya";
+		
+		if(name1.compareTo(name2) == 0){
+			System.out.println("The string is equal");
+		}
+			else{
+				System.out.println("The string is not equal");
+			}
+		}
+	}
+
+		
+		
